a48a8388c540a9b9d8d9d86441d2fee52649b93a
joshmyles <joshmyles844@gmail.com>
2024-05-05 05:47:33 -0700

    1. Console

--- console.py
+++ console.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
 """ This is the entry point to the command prompt """
 import cmd
-import json
-from models import storage
-from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """ Command Interpreter Class """
-
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """Command to exit program """
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
-        """Command to exit program """
+        """EOF command to exit the program"""
         print("")
         return True
 
     def emptyLine(self):
-        """When an empty line is entered """
+        """An empty line is entered"""
         pass
 
 
