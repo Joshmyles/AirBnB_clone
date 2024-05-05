f201dacfefc3385bc0d8a6821041dfa2e74a4505
joshmyles <joshmyles844@gmail.com>
2024-05-05 03:11:44 -0700

    Base model and test class

--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,48 @@
+#!/usr/bin/python3
+
+"""
+Basemodel does initialiazation, serialization & deserialization of instances.
+"""
+
 import uuid
 from datetime import datetime
+import models
+
 
 class BaseModel:
-    """ BaseModel takes care of the initialization, serialization and deserialization of all future instances
+    """ BaseModel for all classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        """ Constructor """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    d_val = datetime.strptime(value, '5Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, d_val)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            models.storage.new(self)
+
     def __str__(self):
-        return"[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """ String Representation """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """ Save method, it updates the updated_at """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
+        """ Returns the dictionary representation of an instance """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.name__
         obj_dict['created_at'] = self.created_at.isoformat()
--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from ..models.base_model import BaseModel
+from models.base_model import BaseModel
 
 """test_base_model"""
 my_model = BaseModel()
@@ -13,4 +13,4 @@ my_model_json = my_model.to_dict()
 print(my_model_json)
 print("JSON of my_model:")
 for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+    print(f"\t{key}: ({type(my_model_json[key])}) - {my_model_json[key]}")
